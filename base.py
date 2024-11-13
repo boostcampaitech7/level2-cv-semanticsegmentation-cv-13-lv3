d93188ec1a3baca941319e8cc8c3eca7f679abb1
boyamie <bhkim4550@pusan.ac.kr>
2024-11-13 14:44:29 +0900

    ✨ FEAT: Update EDA

---
+++ base.py
@@ -0,0 +1,213 @@
+import segmentation_models_pytorch as smp
+from torch.utils.data import Dataset, DataLoader
+import albumentations as A
+import datetime
+from tqdm.auto import tqdm
+import argparse
+import cv2
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from albumentations.pytorch import ToTensorV2
+import os
+
+from dataset import XRayDataset
+from utils import *
+
+CLASSES = [
+    'finger-1', 'finger-2', 'finger-3', 'finger-4', 'finger-5',
+    'finger-6', 'finger-7', 'finger-8', 'finger-9', 'finger-10',
+    'finger-11', 'finger-12', 'finger-13', 'finger-14', 'finger-15',
+    'finger-16', 'finger-17', 'finger-18', 'finger-19', 'Trapezium',
+    'Trapezoid', 'Capitate', 'Hamate', 'Scaphoid', 'Lunate',
+    'Triquetrum', 'Pisiform', 'Radius', 'Ulna',
+    ]
+
+def save_eda_images(dataset, save_dir, num_examples=5):
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    
+    for i in range(num_examples):
+        # Get a random sample from the dataset
+        image, mask = dataset[i]  # Unpack the tuple directly
+
+        # Convert images to saveable format
+        image_np = (image.cpu().numpy().transpose(1, 2, 0) * 255).astype(np.uint8)
+        mask_np = (mask.cpu().numpy().transpose(1, 2, 0) * 255).astype(np.uint8)
+
+        # Save image and mask with index
+        cv2.imwrite(os.path.join(save_dir, f"image_{i}.png"), image_np)
+        cv2.imwrite(os.path.join(save_dir, f"mask_{i}.png"), mask_np)
+
+def main(seed, epochs, lr, batch_size, valid_batch_size, valid_interval, valid_thr,
+         save_dir, save_name, encoder_name, encoder_weights, clahe, cp):
+    
+    image_root = "/data/ephemeral/home/train/DCM"  # Update to correct image directory
+    label_root = "/data/ephemeral/home/train/outputs_json"  # Update to correct label directory
+
+    set_seed(seed)
+    
+    save_dir_root = '/data/ephemeral/home'
+    if not os.path.isdir(save_dir_root):
+        os.mkdir(save_dir_root)
+        
+    save_dir = os.path.join(save_dir_root, save_dir)
+    if not os.path.isdir(save_dir):                                                           
+        os.mkdir(save_dir)
+
+    ### MODEL SETTING ###
+    model = smp.DeepLabV3Plus(
+                encoder_name=encoder_name,
+                encoder_weights=encoder_weights,
+                in_channels=3,
+                classes=len(CLASSES)
+            )
+
+    train_tf = A.Resize(1024, 1024)
+    valid_tf = A.Resize(1024, 1024)
+    
+    # Initialize datasets with image and label root directories
+    train_dataset = XRayDataset(image_root=image_root, label_root=label_root, is_train=True, transforms=train_tf, clahe=clahe, copypaste=cp)
+    valid_dataset = XRayDataset(image_root=image_root, label_root=label_root, is_train=False, transforms=valid_tf, clahe=clahe, copypaste=False)
+
+    # EDA - Save sample augmented images
+    eda_dir = '/data/ephemeral/home/eda'
+    save_eda_images(train_dataset, eda_dir)
+
+    train_loader = DataLoader(
+        dataset=train_dataset, 
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=4,
+        drop_last=True,
+    )
+
+    valid_loader = DataLoader(
+        dataset=valid_dataset, 
+        batch_size=valid_batch_size,
+        shuffle=False,
+        num_workers=1,
+        drop_last=False
+    )
+    
+    # Loss function 정의
+    criterion = nn.BCEWithLogitsLoss()
+
+    # Optimizer 정의
+    optimizer = optim.Adam(params=model.parameters(), lr=lr, weight_decay=1e-6)
+    
+    # Scheduler 정의
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs, eta_min=0)  # T_max는 최대 에포크 수, eta_min은 최소 학습률
+    
+    print(f'Start training..')
+    
+    n_class = len(CLASSES)
+    best_dice = 0.
+    
+    for epoch in range(epochs):
+        model.train()
+        total_train_loss = 0
+        for step, (images, masks) in enumerate(train_loader):            
+            # gpu 연산을 위해 device 할당
+            images, masks = images.cuda(), masks.cuda()
+            model = model.cuda()
+            
+            # inference
+            outputs = model(images)
+            
+            # loss 계산
+            loss = criterion(outputs, masks)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            total_train_loss += loss
+            
+            # step 주기에 따른 loss 출력
+            if (step + 1) % 25 == 0:
+                print(
+                    f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} | '
+                    f'Epoch [{epoch+1}/{epochs}], '
+                    f'Step [{step+1}/{len(train_loader)}], '
+                    f'Loss: {round(loss.item(),4)}'
+                )
+                
+        scheduler.step()        
+        
+        total_train_loss = total_train_loss.item()
+        mean_train_loss = total_train_loss/len(train_loader)
+          
+        # validation 주기에 따른 loss 출력 및 best model 저장
+        if (epoch + 1) % valid_interval == 0:
+            print(f'Start validation #{epoch:2d}')
+            set_seed(seed)
+            model.eval()
+
+            dices = []
+            with torch.no_grad():
+                n_class = len(CLASSES)
+                total_valid_loss = 0
+                cnt = 0
+
+                for step, (images, masks) in tqdm(enumerate(valid_loader), total=len(valid_loader)):
+                    images, masks = images.cuda(), masks.cuda()         
+                    model = model.cuda()
+                    
+                    outputs = model(images)
+                    if outputs.size(-2) != masks.size(-2) or outputs.size(-1) != masks.size(-1):
+                        outputs = F.interpolate(outputs, size=masks.size()[-2:], mode="bilinear")
+                    
+                    loss = criterion(outputs, masks)
+                    total_valid_loss += loss
+                    cnt += 1
+                    
+                    outputs = torch.sigmoid(outputs)
+                    outputs = (outputs > valid_thr).detach().cpu()
+                    masks = masks.detach().cpu()
+                    
+                    dice = dice_coef(outputs, masks)
+                    dices.append(dice)
+                    
+            total_valid_loss = total_valid_loss.item()
+            mean_valid_loss = total_valid_loss / len(valid_loader)
+                        
+            dices = torch.cat(dices, 0)
+            dices_per_class = torch.mean(dices, 0)
+            dice_str = [
+                f"{c:<12}: {d.item():.4f}"
+                for c, d in zip(CLASSES, dices_per_class)
+            ]
+            dice_str = "\n".join(dice_str)
+            print(dice_str)
+            
+            avg_dice = torch.mean(dices_per_class).item()
+            
+            if best_dice < avg_dice:
+                print(f"Best performance at epoch: {epoch + 1}, {best_dice:.4f} -> {avg_dice:.4f}")
+                print(f"Save model in {save_dir}")
+                best_dice = avg_dice
+                save_model(model, save_dir=save_dir, file_name=save_name)
+                
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--seed', type=int, default=137, help='random seed (default: 21)')
+    parser.add_argument('--epochs', type=int, default=100, help='number of epochs to train (default: 200)')
+    parser.add_argument('--lr', type=float, default=1e-4, help='learning rate (default: 1e-4)')
+    parser.add_argument('--batch_size', type=int, default=2, help='input batch size for training (default: 8->2)')
+    parser.add_argument('--valid_batch_size', type=int, default=2, help='input batch size for validating (default: 2)')
+    parser.add_argument('--valid_interval', type=int, default=2, help='validation after {valid_interval} epochs (default: 10)')
+    parser.add_argument('--valid_thr', type=float, default=.5, help='validation threshold (default: 0.5)')
+   
+    # Removed image_root and label_root from arguments
+    parser.add_argument('--save_dir', type=str, default="exp", help="model save directory")
+    parser.add_argument('--save_name', type=str, default="best.pt", help="model save name")
+    
+    parser.add_argument('--encoder_name', type=str, default='tu-xception71', help="encoder name")
+    parser.add_argument('--encoder_weights', type=str, default="imagenet", help="pre-trained weights for encoder initialization")
+    parser.add_argument('--clahe', type=bool, default=False, help='clahe augmentation')
+    parser.add_argument('--cp', type=bool, default=False, help='copypaste augmentation')
+    
+    args = parser.parse_args()
+    main(**args.__dict__)
---
+++ dataset.py
@@ -0,0 +1,254 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torchvision import models
+
+from sklearn.model_selection import GroupKFold
+import numpy as np
+import os
+import cv2
+import json
+
+from PIL import Image, ImageDraw
+import random
+
+CLASSES = [
+    'finger-1', 'finger-2', 'finger-3', 'finger-4', 'finger-5',
+    'finger-6', 'finger-7', 'finger-8', 'finger-9', 'finger-10',
+    'finger-11', 'finger-12', 'finger-13', 'finger-14', 'finger-15',
+    'finger-16', 'finger-17', 'finger-18', 'finger-19', 'Trapezium',
+    'Trapezoid', 'Capitate', 'Hamate', 'Scaphoid', 'Lunate',
+    'Triquetrum', 'Pisiform', 'Radius', 'Ulna',
+]
+
+CLASS2IND = {v: i for i, v in enumerate(CLASSES)}
+IND2CLASS = {v: k for k, v in CLASS2IND.items()}
+
+def do_clahe(image) :
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+    clahe = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(8,8)) #default 2.0
+    image[:,:,0] = clahe.apply(image[:,:,0])
+    image = cv2.cvtColor(image, cv2.COLOR_YUV2BGR)
+    return image
+
+class XRayDataset(Dataset):
+    def __init__(self, image_root, label_root, is_train=True, transforms=None, clahe=False, copypaste=False, k=3):
+        pngs = {
+            os.path.relpath(os.path.join(root, fname), start=image_root)
+            for root, _dirs, files in os.walk(image_root)
+            for fname in files
+            if os.path.splitext(fname)[1].lower() == ".png"
+        }
+        
+        jsons = {
+            os.path.relpath(os.path.join(root, fname), start=label_root)
+            for root, _dirs, files in os.walk(label_root)
+            for fname in files
+            if os.path.splitext(fname)[1].lower() == ".json"
+        }
+
+        # Debugging print statements
+        print(f"Found {len(pngs)} .png files in {image_root}")
+        print(f"Found {len(jsons)} .json files in {label_root}")
+
+        if len(pngs) == 0 or len(jsons) == 0:
+            raise ValueError("No .png or .json files found. Check if paths are correct and files are in the specified directories.")
+
+        jsons_fn_prefix = {os.path.splitext(fname)[0] for fname in jsons}
+        pngs_fn_prefix = {os.path.splitext(fname)[0] for fname in pngs}
+
+        assert len(jsons_fn_prefix) - len(pngs_fn_prefix) == 0, "Mismatch in .json and .png file counts"
+        assert len(pngs_fn_prefix) - len(jsons_fn_prefix) == 0, "Mismatch in .png and .json file counts"
+
+        pngs = sorted(pngs)
+        jsons = sorted(jsons)
+        
+        _filenames = np.array(pngs)
+        _labelnames = np.array(jsons)
+        
+
+        groups = [os.path.dirname(fname) for fname in _filenames]
+
+        ys = [0 for fname in _filenames]
+
+        gkf = GroupKFold(n_splits=5)
+        
+        filenames = []
+        labelnames = []
+        dataset_no = 2
+        for i, (x, y) in enumerate(gkf.split(_filenames, ys, groups)):
+            if is_train:
+                # 0번을 validation dataset으로 사용합니다.
+                if i == dataset_no:
+                    continue
+                    
+                filenames += list(_filenames[y])
+                labelnames += list(_labelnames[y])
+            
+            else:
+                if i != dataset_no :
+                    continue
+                filenames = list(_filenames[y])
+                labelnames = list(_labelnames[y])
+                
+                # skip i > 0
+                break
+        
+        self.filenames = filenames
+        self.labelnames = labelnames
+        self.is_train = is_train
+        self.transforms = transforms
+        self.image_root = image_root
+        self.label_root = label_root 
+        
+        self.clahe = clahe
+        self.copypaste = copypaste
+        self.k = k
+    
+    def __len__(self):
+        return len(self.filenames)
+    
+    def get_coord(self, polygon):
+        polygon = polygon
+        for i in range(len(polygon)):
+            polygon[i] = tuple(polygon[i])
+        polygon_np = np.array(polygon)
+        
+        max = np.max(polygon_np, axis=0)
+        min = np.min(polygon_np, axis=0)
+        
+        return max, min
+    
+    def __getitem__(self, item):
+        image_name = self.filenames[item]
+        image_path = os.path.join(self.image_root, image_name)
+        
+        image = cv2.imread(image_path)
+        
+        if self.clahe :
+            image = do_clahe(image)
+            
+        image = image / 255.
+        
+        label_name = self.labelnames[item]
+        label_path = os.path.join(self.label_root, label_name)
+        
+        # process a label of shape (H, W, NC)
+        label_shape = tuple(image.shape[:2]) + (len(CLASSES), )
+        label = np.zeros(label_shape, dtype=np.uint8)
+        
+        # read label file
+        with open(label_path, "r") as f:
+            annotations = json.load(f)
+        annotations = annotations["annotations"]
+        
+        # iterate each class
+        for ann in annotations:
+            c = ann["label"]
+            class_ind = CLASS2IND[c]
+            points = np.array(ann["points"])
+            
+            # polygon to mask
+            class_label = np.zeros(image.shape[:2], dtype=np.uint8)
+            cv2.fillPoly(class_label, [points], 1)
+            label[..., class_ind] = class_label
+        
+        if self.copypaste:
+            randoms = random.choices([i for i in range(640)], k=self.k)
+            for i in randoms:
+                target_image = cv2.imread(os.path.join(self.image_root, self.filenames[i])) / 255.
+                target_label_path = os.path.join(self.label_root, self.labelnames[i])
+
+                with open(target_label_path, "r") as f:
+                    target_annotations = json.load(f)
+                target_annotations = target_annotations["annotations"]
+
+                for ann in target_annotations:
+                    target_c = ann["label"]
+                    target_c = CLASS2IND[target_c]
+                    if target_c == 19 or target_c == 20 or target_c == 25 or target_c == 26:
+                        points = np.array(ann["points"])
+                        max, min = self.get_coord(ann['points'])
+                        x = random.randint(100,1800)
+                        y = random.randint(100,1800)
+                        alpha = random.randint(25,50)
+
+                        # 0. check whether generated (x,y) coordinate is in the background 
+                        bone_area_x = [i for i in range(400,1600)]
+                        while x in bone_area_x:
+                            x = random.randint(100,1800)
+                        x -= alpha
+                        
+                        # 1. create mask for new image
+                        img = Image.new('L', target_image.shape[:2], 0)
+                        ImageDraw.Draw(img).polygon(ann['points'], outline=0, fill=1)
+                        mask = np.array(img)
+                        
+                        # 2. paste maskout poly to source image
+                        new_image = cv2.bitwise_or(target_image, target_image, mask=mask)
+                        image[y:y+max[1]-min[1], x:x+max[0]-min[0], ...] = new_image[min[1]:max[1], min[0]:max[0], ...]
+
+                        # 3. update label
+                        ori_label = label[..., target_c]
+                        ori_label[y:y+max[1]-min[1], x:x+max[0]-min[0]] = mask[min[1]:max[1], min[0]:max[0]]
+                        label[..., target_c] = ori_label
+
+        if self.transforms is not None:
+            inputs = {"image": image, "mask": label} if self.is_train else {"image": image}
+            result = self.transforms(**inputs)
+            
+            image = result["image"]
+            label = result["mask"] if self.is_train else label
+
+        # to tenser will be done later
+        image = image.transpose(2, 0, 1)    # make channel first
+        label = label.transpose(2, 0, 1)
+        
+        image = torch.from_numpy(image).float()
+        label = torch.from_numpy(label).float()
+            
+        return image, label
+    
+class XRayInferenceDataset(Dataset):
+    def __init__(self, image_root, transforms=None, clahe=False):
+        pngs = {
+            os.path.relpath(os.path.join(root, fname), start=image_root)
+            for root, _dirs, files in os.walk(image_root)
+            for fname in files
+            if os.path.splitext(fname)[1].lower() == ".png"
+        }
+        _filenames = pngs
+        _filenames = np.array(sorted(_filenames))
+        
+        self.filenames = _filenames
+        self.transforms = transforms
+        self.image_root = image_root
+        self.clahe = clahe
+    
+    def __len__(self):
+        return len(self.filenames)
+    
+    def __getitem__(self, item):
+        image_name = self.filenames[item]
+        image_path = os.path.join(self.image_root, image_name)
+        
+        image = cv2.imread(image_path)
+        
+        if self.clahe :
+            image = do_clahe(image)
+            
+        image = image / 255.
+        
+        if self.transforms is not None:
+            inputs = {"image": image}
+            result = self.transforms(**inputs)
+            image = result["image"]
+
+        # to tenser will be done later
+        image = image.transpose(2, 0, 1)    # make channel first
+        
+        image = torch.from_numpy(image).float()
+            
+        return image, image_name
