33d59af9246b1561ea0465630a1d81540f0abc7d
boyamie <bhkim4550@pusan.ac.kr>
2024-11-16 18:02:58 +0900

    ⚰️ FIX: Remove dead code

--- module/trainer.py
+++ module/trainer.py
@@ -2,36 +2,38 @@ import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
 from torchmetrics.classification import MulticlassAccuracy
+from lightning.pytorch import LightningModule
 
-class CustomLightningModule(pl.LightningModule):
-    def __init__(self, model, lr):
+class CustomLightningModule(LightningModule):
+    def __init__(self, model, num_classes, lr):
         super(CustomLightningModule, self).__init__()
         self.model = model
+        self.num_classes = num_classes
         self.lr = lr
-        self.train_acc = MulticlassAccuracy(num_classes=self.model.classes, average="macro")
-        self.valid_acc = MulticlassAccuracy(num_classes=self.model.classes, average="macro")
+        self.train_acc = MulticlassAccuracy(num_classes=num_classes, average="macro")
+        self.val_acc = MulticlassAccuracy(num_classes=num_classes, average="macro")
 
     def forward(self, x):
         return self.model(x)
 
     def training_step(self, batch, batch_idx):
-        images, masks = batch
-        outputs = self.model(images)
-        loss = F.cross_entropy(outputs, masks)
-        self.log("train_loss", loss, prog_bar=True, logger=True)
-        acc = self.train_acc(outputs.argmax(dim=1), masks)
-        self.log("train_acc", acc, prog_bar=True, logger=True)
+        _, images, labels = batch
+        logits = self(images)
+        loss = F.cross_entropy(logits, labels)
+        preds = torch.argmax(logits, dim=1)
+        self.train_acc.update(preds, labels)
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
+        self.log("train_acc", self.train_acc, on_step=True, on_epoch=True, prog_bar=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        images, masks = batch
-        outputs = self.model(images)
-        loss = F.cross_entropy(outputs, masks)
-        self.log("val_loss", loss, prog_bar=True, logger=True)
-        acc = self.valid_acc(outputs.argmax(dim=1), masks)
-        self.log("val_acc", acc, prog_bar=True, logger=True)
-        return loss
+        _, images, labels = batch
+        logits = self(images)
+        loss = F.cross_entropy(logits, labels)
+        preds = torch.argmax(logits, dim=1)
+        self.val_acc.update(preds, labels)
+        self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("val_acc", self.val_acc, on_step=False, on_epoch=True, prog_bar=True)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
-        return optimizer
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
