ad270d7dcab040b38dafec2b335e4cc130d93044
boyamie <bhkim4550@pusan.ac.kr>
2024-11-18 14:00:05 +0900

    📝 TEST: U-Net++ resnet152, 300epoch

--- model.py
+++ model.py
@@ -3,9 +3,9 @@ from constants import CLASSES
 import segmentation_models_pytorch as smp
 
 def load_model():
-    return smp.Unet(
-        encoder_name="resnet50", # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
-        encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
+    return smp.UnetPlusPlus(
+        encoder_name='resnet152',
+        encoder_weights='imagenet',
         in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
         classes=len(CLASSES),                     # model output channels (number of classes in your dataset)
     )
