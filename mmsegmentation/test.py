311751e66a10db06c24c262fba6a5bcd47a74f77
Ki Hun Sung <staragnarok@gmail.com>
2024-11-20 02:03:23 +0900

    ⚡️UPD: remove argparser, added config into yaml_config, test ckpt path setting improved

--- mmsegmentation/test.py
+++ mmsegmentation/test.py
@@ -6,8 +6,6 @@ from omegaconf import OmegaConf
 from utils.utils import get_sorted_files_by_type, encode_mask_to_rle
 from constants import IND2CLASS
 
-sys.path.append('../mmseg/')
-
 # Copyright (c) OpenMMLab. All rights reserved.
 import os
 import os.path as osp
@@ -36,12 +34,14 @@ def set_xraydataset(config):
 
 def set_yaml_config_test(config, yaml_config):
 
+    cfg_name = osp.splitext(osp.basename(yaml_config.config))[0]
+
     if yaml_config.tta is True:
         config.test_dataloader.dataset.pipeline = config.tta_pipeline
         config.tta_model.module = config.model
         config.model = config.tta_model
 
-    ckpt_filepath = os.path.join(yaml_config.checkpoint_dir, osp.basename(yaml_config.checkpoint_file) + '.pth')
+    ckpt_filepath = os.path.join(yaml_config.checkpoint_dir, cfg_name, yaml_config.checkpoint_file.rsplit('.', 1)[0] + '.pth')
     config.load_from = ckpt_filepath
     
     return config
@@ -64,25 +64,22 @@ def inference(args, rles, filename_and_class, thr=0.5):
 
 
 def test(args, yaml_cfg):
+
+    cfg_name = osp.splitext(osp.basename(yaml_cfg.config))[0]
     # load config
-    cfg = Config.fromfile(args.config)
+    cfg = Config.fromfile(yaml_cfg.config)
   
     cfg = set_xraydataset(cfg)
     cfg = set_yaml_config_test(cfg, yaml_cfg)
 
     cfg.launcher = args.launcher
 
-    # work_dir is determined in this priority: CLI > segment in file > filename
-    if args.work_dir is not None:
-        # update configs according to CLI args if args.work_dir is not None
-        cfg.work_dir = args.work_dir
-    elif cfg.get('work_dir', None) is None:
+    if cfg.get('work_dir', None) is None:
         # use config filename as default work_dir if cfg.work_dir is None
-        cfg.work_dir = osp.join('./work_dirs',
-                                osp.splitext(osp.basename(args.config))[0])
+        cfg.work_dir = osp.join('./work_dirs', cfg_name)
 
-    if args.show or args.show_dir:
-        cfg = trigger_visualization_hook(cfg, args)
+    # if args.show or args.show_dir:
+    #     cfg = trigger_visualization_hook(cfg, args)
 
     # add output_dir in metric
     cfg.test_evaluator['keep_results'] = True
