60dfe9bba96e28dc1853a2ac4cd93d766e8c8a97
Jin-SukKim <99wlstjr@gmail.com>
2024-11-21 17:13:04 +0900

    FEAT: edge detection and ColorJitter

--- lightningmodule/augmentation.py
+++ lightningmodule/augmentation.py
@@ -47,10 +47,37 @@ class ColorJitterAugmentation(A.ImageOnlyTransform):
         
         return image
 
+class EdgeDetection(A.ImageOnlyTransform):
+    def __init__(self, threshold1=100, threshold2=200, p=1.0):
+        super().__init__(always_apply=False, p=p)
+        self.threshold1 = threshold1
+        self.threshold2 = threshold2
+        
+    def apply(self, image, **kwargs):
+        # Convert to uint8 for cv2
+        image = (image * 255).astype(np.uint8)
+        
+        # Canny Edge Detection 적용
+        # threshold1 : 하단 임계값, threshold2 : 상단 임계값 (보통 1:2 또는 1:3 비율로 설정)
+        # 임계값이 높으면 밝기 차이가 큰 edge만 검출, 낮으면 밝기 차이가 적은 edge도 검출
+        edges = cv2.Canny(image, threshold1=self.threshold1, threshold2=self.threshold2)
+        
+        # edges to 3-channel image 변환
+        edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
+        
+        # float64 [0,1] range로 변환
+        edges = edges.astype(np.float64) / 255.0
+        return edges
+
 def load_transforms(args):
     transform = [
-        ColorJitterAugmentation(brightness=0.1, contrast=0.4, p=1.0),
         A.Resize(args.input_size, args.input_size),
+        A.Scale(scale_limit=(0.8, 1.2), p=1.0),
+        # ColorJitter를 사용하려면 ToFloat 사용해야됨
+        # A.ToFloat(max_value=255),
+        # A.ColorJitter(brightness=(1.1, 1.1), contrast=(1.4, 1.4), saturation=0, hue=0, p=1.0),
+        # ColorJitterAugmentation(brightness=0.1, contrast=0.4, p=1.0),
+        # EdgeDetection(threshold1=50, threshold2=100, p=1.0),
     ]
     
     if args.clahe:
@@ -60,5 +87,7 @@ def load_transforms(args):
         clahe_aug = CLAHEAugmentation(clip_limit=clahe_clip_limit, tile_grid_size=clahe_tile_grid_size)
         transform.append(clahe_aug.albumentations_clahe())
         
+    # transform.append(A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])) # ImageNet Default 정규화
+    transform.append(A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])) 
     transform = A.Compose(transform)
     return transform
