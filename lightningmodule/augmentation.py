fbf97c82392facce0b8679b125720f63bdaf875c
Jin-SukKim <99wlstjr@gmail.com>
2024-11-22 00:05:45 +0900

    UPDATE: find augmentation parameters

--- lightningmodule/augmentation.py
+++ lightningmodule/augmentation.py
@@ -71,13 +71,29 @@ class EdgeDetection(A.ImageOnlyTransform):
 
 def load_transforms(args):
     transform = [
+        # A.RandomScale(scale_limit=(0.9, 1.1), p=1.0),
+        # X-ray에 적합한 Affine 변환
+        A.Affine(
+            scale=(0.9, 1.1),           # 급격한 크기 변화 방지
+            translate_percent={
+                'x': (-0.07, 0.07),     # x축 이동
+                'y': (-0.07, 0.07)      # y축 이동
+            },
+            rotate=(-10, 10),           # 의료영상은 급격한 회전 지양
+            shear=(-7, 7),              # 약간의 전단 변환
+            interpolation=cv2.INTER_LINEAR,
+            mask_interpolation=cv2.INTER_NEAREST,
+            cval=0,                     # 빈 공간을 검은색으로
+            fit_output=True,            # 이미지가 잘리지 않도록 조정
+            p=1                         # 50% 확률로 적용
+        ),
         A.Resize(args.input_size, args.input_size),
-        A.Scale(scale_limit=(0.8, 1.2), p=1.0),
         # ColorJitter를 사용하려면 ToFloat 사용해야됨
         # A.ToFloat(max_value=255),
         # A.ColorJitter(brightness=(1.1, 1.1), contrast=(1.4, 1.4), saturation=0, hue=0, p=1.0),
         # ColorJitterAugmentation(brightness=0.1, contrast=0.4, p=1.0),
-        # EdgeDetection(threshold1=50, threshold2=100, p=1.0),
+        # EdgeDetection(threshold1=100, threshold2=250, p=1.0),
+        # A.HorizontalFlip(p=1.0),
     ]
     
     if args.clahe:
@@ -87,7 +103,7 @@ def load_transforms(args):
         clahe_aug = CLAHEAugmentation(clip_limit=clahe_clip_limit, tile_grid_size=clahe_tile_grid_size)
         transform.append(clahe_aug.albumentations_clahe())
         
-    # transform.append(A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])) # ImageNet Default 정규화
-    transform.append(A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])) 
+    # transform.append(A.Normalize(normalization='min_max', p=1.0))
+    # A.Normalize(normalization='robust', p=1.0), # Percentile Normalization : 매우 밝거나 어두운 영역을 좀 무시하고 정규화
     transform = A.Compose(transform)
     return transform
