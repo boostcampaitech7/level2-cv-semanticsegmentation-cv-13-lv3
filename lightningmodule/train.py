5466e7ec8a3547d8cc05dc768ef9032d6da182cc
jyp <singiri129@jbnu.ac.kr>
2024-11-22 12:07:35 +0900

    [#72]Feat:constants, train.py 수정

--- lightningmodule/train.py
+++ lightningmodule/train.py
@@ -69,10 +69,7 @@ def train_model(args):
     train_dataset = XRayDataset(
         image_files=train_files['filenames'],
         label_files=train_files['labelnames'],
-        transforms = A.Compose([
-        A.CLAHE(clip_limit=3.0, tile_grid_size=(8, 8), p=1.0),  # CLAHE 적용
-        A.Resize(args.input_size, args.input_size)  # Resize
-        ]))
+        transforms = train_transforms
     valid_dataset = XRayDataset(
         image_files=valid_files['filenames'],
         label_files=valid_files['labelnames'],
